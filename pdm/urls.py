cebbb8fe2f7a1caef956eb67073eeb4c28accbaa
Jonatan Vianna <jonatan.vianna@gmail.com>
2018-04-17 00:54:15 -0300

    portal adjust

--- pdm/urls.py
+++ pdm/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
+    url(r'^$', views.home, name='pdm_home'),
     url(r'^resultado/$', views.busca_log, name='busca_log'),
 ]
--- tools_adm/settings.py
+++ tools_adm/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'storages',
     'portal',
     'diretiva',
+    'pdm',
 ]
 
 MIDDLEWARE = [
