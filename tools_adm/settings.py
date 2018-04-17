51ea9cfb1a1da7f2783783e011f35e4d18171590
Jonatan Vianna <jonatan.vianna@gmail.com>
2018-04-16 23:49:28 -0300

    portal adjust

--- tools_adm/settings.py
+++ tools_adm/settings.py
@@ -26,7 +26,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'q_@+oxyqk*jk=6+(ydv#tsdq(-x1vx!(e7k(653)$=_6!&t_zm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
